f20f8092dbc038c1ce9ffb5a7382d2a199171550
ravi0228 <ravisun1320@gmail.com>
2023-05-03 14:36:39 +0530

    first commit

---
+++ db.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "db_server" {
+  count = var.db_want == true ? var.db_count : 0
+  ami           = "ami-03a933af70fa97ad2"
+  instance_type = var.instance_type
+  key_name      = var.my_key_name
+  
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file(var.my_local_aws_private_key_path)
+    host     = self.public_ip
+  }
+
+  tags = {
+    Name = "Db_server-${count.index}"
+  }  
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apache2",
+      "sudo systemctl start apache2",
+      "sudo systemctl enable apache2",
+      "cd /var/www/html",
+      "sudo chmod 407 index.html",
+      "sudo echo '<h1>Hello World from ravi Database_Server</h1>' > /var/www/html/index.html",
+      "sudo systemctl restart apache2"
+    ]
+
+  }  
+}
---
+++ route53.tf
@@ -0,0 +1,44 @@
+resource "aws_route53_zone" "hosted_zone" {
+  name = var.my_domain_name
+}
+
+locals {
+  web_server_ip = aws_instance.web_server.*.public_ip
+  mail_server_ip = aws_instance.mail_server.*.public_ip
+  db_server_ip = aws_instance.db_server.*.public_ip
+  httpd_server_ip = aws_instance.httpd_server.*.public_ip
+
+}
+
+
+resource "aws_route53_record" "mail" {
+  zone_id = resource.aws_route53_zone.hosted_zone.zone_id
+  name    = var.my_mail_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mail_server[0].public_ip]
+}
+
+resource "aws_route53_record" "db" {
+  zone_id = resource.aws_route53_zone.hosted_zone.zone_id
+  name    = var.my_db_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.db_server[0].public_ip]
+}
+
+resource "aws_route53_record" "httpd" {
+  zone_id = resource.aws_route53_zone.hosted_zone.zone_id
+  name    = var.my_httpd_server_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.httpd_server[0].public_ip]
+}
+
+resource "aws_route53_record" "web" {
+  zone_id = resource.aws_route53_zone.hosted_zone.zone_id
+  name    = var.my_web_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.web_server[0].public_ip]
+}
