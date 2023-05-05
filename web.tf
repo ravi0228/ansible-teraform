1af1fe3926fa5dff08d205b3304d2f05fea56e34
ravi0228 <113755342+ravi0228@users.noreply.github.com>
2023-05-05 16:40:30 +0530

    Update web.tf

--- web.tf
+++ web.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_server" {
   count = var.web_want == true ? var.web_count : 0
-  ami           = "ami-0aa2b7722dc1b5612"
+  ami           = "ami-007855ac798b5175e"
   instance_type = var.instance_type
   key_name      = var.my_key_name
   
